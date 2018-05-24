692d30dcd33e2094519d77b9513009b0f4092459
mshehan <mshehan@my.smccd.edu>
2018-05-24 01:35:19 -0700

    answered questions from HW

--- read.py
+++ read.py
@@ -4,17 +4,65 @@ import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 import string
+import time
+from nltk.stem.porter import PorterStemmer
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 remove = stopwords.words()
-print remove
-
-print string.punctuation
+stemmer = PorterStemmer()
 
 train = open("train_file_cmps142_hw3", "r")
+print "starting preprocessing..."
+start = time.clock()
+
+#need to get number of distinct tokens in the train set
+# tokens = word_tokenize(train.read().lower())
+# filtered_tokens = [word for word in tokens if word not in remove and word not in string.punctuation]
+# stemmed_tokens = [stemmer.stem(words.decode('utf-8')) for words in filtered_tokens]
+
+# distinct_tokens = dict.fromkeys(set(stemmed_tokens), 0)
+
+# for word in stemmed_tokens:
+# 	distinct_tokens[word] += 1
+
+# vocabulary = stemmed_tokens
+
+# for word in stemmed_tokens:
+# 	if distinct_tokens[word] < 5:
+# 		vocabulary.remove(word)
+
+# vocabulary = set(vocabulary)
+# print "the size of the vocabulary is " + str(len(vocabulary)) 
+# print distinct_tokens
+
+distinct_tokens = dict()
+corpus = []
 for line in train:
+	
 	tokens = word_tokenize(line.lower())
-	tokens2 = [x for x in tokens if x not in remove and x not in string.punctuation]
-	print tokens2;
+	filtered_tokens = [word for word in tokens if word not in remove and word not in string.punctuation]
+	
+	stemmed_tokens = [stemmer.stem(words.decode('utf-8')) for words in filtered_tokens]
+	for word in stemmed_tokens:
+		if word in distinct_tokens.keys():
+			distinct_tokens[word] += 1
+		else:
+			distinct_tokens[word] = 0
+	corpus.append(stemmed_tokens)
+
+
+for line in corpus:
+	for word in line:
+		if distinct_tokens[word] < 5:
+			line.remove(word)
+
+#requires step 7 and check on validity of lines 56 - 59 
+			
+
+
+
+
+end = time.clock()
+print "ended preprocessing in " + str(end-start) + " seconds"
