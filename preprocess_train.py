87bf46a3461ac7850b5dc6f936aff0800ad29e7c
mshehan <mshehan@my.smccd.edu>
2018-05-26 00:23:41 -0700

    I have something and it looks like a csv

--- preprocess_train.py
+++ preprocess_train.py
@@ -12,11 +12,11 @@ sys.setdefaultencoding('utf-8')
 remove = stopwords.words()
 stemmer = PorterStemmer()
 
-def getLabels(in_stream):
+def generate_labels_list(in_stream):
 	in_stream.seek(0)
 	labels = []
 	for line in in_stream:
-		labels.append(line[line.find('\t')])
+		labels.append(line[:line.find('\t')])
 	return labels
 
 def remove_irrelevant_words(vocabulary):
@@ -25,7 +25,7 @@ def remove_irrelevant_words(vocabulary):
 			del vocabulary[word]
 	return vocabulary
 
-def prepare_corpus(corpus, tokens):
+def fill_corpus_with_zeros(corpus, tokens):
 	new_corpus = []
 	for line in corpus:
 		new_corpus.append(dict())
@@ -34,21 +34,44 @@ def prepare_corpus(corpus, tokens):
 				new_corpus[-1][word] = tokens[word]
 	return new_corpus
 
-def featurify(corpus, vocabulary, labels):
+def feature_list_to_csv(corpus, vocabulary, labels):
 	csv_array = []
-	csv_array.append(vocabulary.keys())
-	csv_array[0].append('label')
 	line_num = 0
+
 	for line in corpus:
 		for word,count in vocabulary.items():
 			if word not in line:
 				line[word] = 0
-			csv_array.append(line.keys())
-			csv_array[-1].append(labels[line_num])
-			line_num += 1
+		line.update({'label': labels[line_num]})
+		csv_array.append(line)
+		line_num += 1
 	
+	first_line = csv_array[0].keys()
+	first_line.append('label')
+	csv_array.insert(0, first_line)
 	return csv_array
 	
+def filter_tokens(line):
+	line_without_label = line[line.find('\t')+1:]
+	tokens = word_tokenize(line_without_label.lower())
+
+	filtered_tokens = [word for word in tokens if (word not in remove) and (word not in string.punctuation)]
+	stemmed_tokens = [stemmer.stem(unicode(words)) for words in filtered_tokens]
+	return stemmed_tokens
+
+def write_to_csv(outfile, bag_of_words):
+
+	for key in bag_of_words[0]:
+		outfile.write("\"%s\"," % key)
+	outfile.write('\n')
+	keys = bag_of_words.pop(0)
+	
+	for line in bag_of_words:
+		for key in keys:
+			outfile.write("%s," % line[key])
+		outfile.write("\n")
+
+
 
 def preprocess_train_set(in_stream):
 	corpus = []
@@ -58,44 +81,34 @@ def preprocess_train_set(in_stream):
 	in_stream.seek(0)
 	
 	for line in in_stream:
-		line_without_label = line[line.find('\t')+1:]
-		tokens = word_tokenize(line_without_label.lower())
-
-		filtered_tokens = [word for word in tokens if (word not in remove) and (word not in string.punctuation)]
-		stemmed_tokens = [stemmer.stem(unicode(words)) for words in filtered_tokens]
+		
+		stemmed_tokens = filter_tokens(line)
 		corpus.append(stemmed_tokens)
 		for word in stemmed_tokens:
 			if word in distinct_tokens:
 				distinct_tokens[word] += 1
 			else:
 				distinct_tokens[word] = 1
-
 	
 	vocabulary = remove_irrelevant_words(distinct_tokens)
-	corpus = prepare_corpus(corpus,vocabulary)
-	labels = getLabels(in_stream)
-	#csv_ready = featurify(corpus,vocabulary,labels)
-	#print vocabulary
-	
+	corpus = fill_corpus_with_zeros(corpus,vocabulary)
+	labels = generate_labels_list(in_stream)
+	csv_ready = feature_list_to_csv(corpus,vocabulary,labels)
 
-	
+	output = open("output_file.csv", "w");
 
+	write_to_csv(output,csv_ready)
 
-
-
+	output.close()
 
 def _main_():
 	file = open('./train_file_cmps142_hw3', 'r')
 	#print "starting preprocessing..."
 	start = time.clock()
 	
-	#preprocess_train_set(file)
+	preprocess_train_set(file)
 
-	test = {'a': 1, 'b':2, 'c': 3}
-	test2 = []
-	test2.append(test.keys())
-
-	print test2[-1]
+	file.close()
 	
 	end = time.clock()
 	#print "ended preprocessing in " + str(end-start) + " seconds"
